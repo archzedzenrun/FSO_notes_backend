c883ae5f717ea805df63fc6ac9f620e1ae3e016e
archzedzenrun <archzedzenrun@gmail.com>
2024-07-11 13:55:22 -0700

    remove mongo file, add error message

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-let notes = [
-  {
-    id: '1',
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: '2',
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: '3',
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes);
@@ -62,21 +44,10 @@ app.put('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  // const id = request.params.id;
-  // notes = notes.filter(note => note.id !== id);
-
-  // response.status(204).end();
   Note.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
   }).catch(error => next(error));
 })
-
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => Number(n.id)))
-//     : 0
-//   return String(maxId + 1)
-// }
 
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
