e252eade235281940050163d7ad9f99f57bd0094
archzedzenrun <archzedzenrun@gmail.com>
2024-07-11 12:55:49 -0700

    db

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://archzedzenrun:${password}@cluster0.cbsakxz.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  _id: 655,
+  content: 'Mabongo',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log(`${note} saved!`)
+  mongoose.connection.close()
+})
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note);
+//   })
+
+//   mongoose.connection.close();
+// })
